06968c33c6fc152a21f56ebf012606079056180f
Mae <mae@bsbo.com.au>
2021-03-18 09:15:09 +0800

    added test pagination

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  currentData;
+  testItems = [
+    {
+      id:1,
+      title:"test1",
+      isOpen:false
+    },
+    {
+      id:2,
+      title:"test2",
+      isOpen:false
+    },
+    {
+      id:3,
+      title:"test3",
+      isOpen:false
+    },
+    {
+      id:4,
+      title:"test4",
+      isOpen:false
+    }
+  ];
+  currentPage = 1;
+  lastPage=0;
+  constructor() {
+    this.lastPage = this.testItems.length;
+    console.log(this.lastPage)
+    this.currentData = this.testItems[0]
+  }
 
-  constructor() {}
+  setCurrentData(){
+    const idx = this.currentPage;
+    this.currentData = this.testItems[idx-1];
+  }
+
+  nextPage(){
+    this.currentPage++;
+    this.setCurrentData()
+  }
+
+  previousPage(){
+    this.currentPage--;
+    this.setCurrentData()
+  }
 
 }
